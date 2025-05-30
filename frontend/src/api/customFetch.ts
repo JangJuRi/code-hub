57e1f5f423a4ae2e2d7ea9eed3abb58d8d262c58
JangJuRi <wkdwnfl119@naver.com>
2025-05-30 09:46:38 +0900

    customFetch 타입 정리

--- frontend/src/api/customFetch.ts
+++ frontend/src/api/customFetch.ts
@@ -5,18 +5,22 @@ type FetchOptions = {
     query?: Record<string, string | number>;
 };
 
-const customFetch = async <T>(endpoint: string, options: FetchOptions = {}): Promise<{
+type ApiResponse<T> = {
     data: any;
     message: string;
     success: boolean;
-}> => {
+};
+
+const customFetch = async <T>(endpoint: string, options: FetchOptions = {}): Promise<ApiResponse<T>> => {
     const { method = 'GET', body, headers = {}, query = {} } = options;
 
     // 토큰 가져오기
     const accessToken = localStorage.getItem("accessToken");
 
     // query 객체가 있으면 URL에 쿼리 문자열을 추가
-    const queryString = new URLSearchParams(query).toString();
+    let queryString: string;
+    // @ts-ignore
+    queryString = new URLSearchParams(query).toString();
     const fullUrl = queryString
         ? `${process.env.NEXT_PUBLIC_API_URL}${endpoint}?${queryString}`
         : `${process.env.NEXT_PUBLIC_API_URL}${endpoint}`;
@@ -46,7 +50,7 @@ const customFetch = async <T>(endpoint: string, options: FetchOptions = {}): Pro
     // 응답 본문이 비어있지 않으면 JSON 파싱
     const textResponse = await response.text(); // 응답을 텍스트로 읽음
     if (!textResponse) {
-        return {}; // 빈 응답일 경우 빈 객체 반환
+        return {data: undefined, message: "", success: false}; // 빈 응답일 경우 빈 객체 반환
     }
 
     try {
@@ -54,13 +58,29 @@ const customFetch = async <T>(endpoint: string, options: FetchOptions = {}): Pro
 
         // 파싱된 결과가 객체인 경우만 반환
         if (parsedResponse && typeof parsedResponse === 'object' && !Array.isArray(parsedResponse)) {
-            return parsedResponse;
+            if (isApiResponse<T>(parsedResponse)) {
+                return parsedResponse;
+            }
         }
     } catch (e) {
         // JSON 파싱 오류가 나면 그대로 텍스트 반환
     }
 
-    return textResponse;
+    if (isApiResponse<T>(textResponse)) {
+        return textResponse;
+    }
+
+    return {data: undefined, message: "", success: false};
+};
+
+const isApiResponse = <T>(obj: any): obj is ApiResponse<T> => {
+    return (
+        obj &&
+        typeof obj === 'object' &&
+        'data' in obj &&
+        'message' in obj &&
+        'success' in obj
+    );
 };
 
 export default customFetch;
